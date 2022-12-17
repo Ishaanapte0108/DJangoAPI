3ed78e8698b49b148e1e4577173c1f742fb463a4
Ishaanapte0108 <ishaanmapte@gmail.com>
2022-12-17 15:28:38 +0530

    editing views file

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,15 +1,19 @@
+import pymongo
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 def index(request):
     return HttpResponse("<h1>Voila</h1>")
 
-import pymongo
-client = pymongo.MongoClient('mongodb+srv://ishaan:masterPassword%400108@trialcluster.spns5qz.mongodb.net/?retryWrites=true&w=majority')
-dbname = client['movies']
-collection=dbname['entertainment']
 
-entertainment_1={
+#import something
+client = pymongo.MongoClient(
+    'mongodb+srv://ishaan:masterPassword%400108@trialcluster.spns5qz.mongodb.net/?retryWrites=true&w=majority')
+dbname = client['movies']
+collection = dbname['entertainment']
+
+entertainment_1 = {
     "name": "The Big Bang Theory",
     "img": "NA",
     "summary": "Comedy Series"
@@ -19,4 +23,3 @@ collection.insert_one(entertainment_1)
 ent_deets = collection.find({})
 for r in ent_deets:
     print(r['name'])
-
